a4244509351c127b5817c44d5d11ab8c49ecff8c
Faye Ly <fayedotly@gmail.com>
2026-01-15 17:05:20 -0800

    finished day 9 part 2

--- day9/day9.py
+++ day9/day9.py
@@ -16,10 +16,38 @@ def area(c1, c2):
 
 coordinates = [line.strip("\n").split(",") for line in open("input.txt")]
 areas = []
+rows = []
 
 for i in range(len(coordinates)):
     for j in range(i+1, len(coordinates)):
         areas.append(area(coordinates[i], coordinates[j]))
 
 areas.sort()
-print(areas[len(areas)-1])
+# print(areas[len(areas)-1])
+
+# part 2
+# 2026-01-15
+# 15:48 to 17:04
+
+import shapely as sl
+
+for i in range(len(coordinates)):
+    coordinates[i] = [int(coord) for coord in coordinates[i]]
+
+def create_box(c1, c2):
+    polygon = sl.Polygon([[c1[0], c2[1]], [c2[0], c2[1]], [c2[0], c1[1]], [c1[0], c1[1]]])
+    return polygon
+    # return sl.box(min(c1[0], c2[0]), min(c1[1], c2[1]), max(c1[0], c2[0]), max(c1[1], c2[1]))
+
+poly = sl.Polygon(coordinates)
+
+largest = 0
+
+for i in range(len(coordinates)):
+    for j in range(i+1, len(coordinates)):
+        box = create_box(coordinates[i], coordinates[j])
+        if sl.within(box, poly):
+            if area(coordinates[i], coordinates[j]) > largest:
+                largest = area(coordinates[i], coordinates[j])
+
+print(largest)
